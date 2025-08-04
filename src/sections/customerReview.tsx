02ae0ecf3ece5982909bdbdeace5094eb8da3c9b
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-08-04 23:52:33 +0300

    customer service and subscribe section

--- src/sections/customerReview.tsx
+++ src/sections/customerReview.tsx
@@ -1,11 +1,34 @@
+import { star } from "../assets/icons";
+import { reviews } from "../constants";
+
 const CustomerReview = () => {
   return (
-    <section>
-      <div>
-        <h2 className="text-center text-">
-          Why Our <span>Customer</span> Choice Us
-        </h2>
-        <p></p>
+    <section className="">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-center text-4xl font-bold">
+          Why Our <span className="text-red-500">Customer</span> Say
+        </h1>
+        <p className="max-w-prose text-center mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis animi
+          aspernatur placeat at saepe quod assumenda fugit earum magni porro,
+          magnam pariatur? Praesentium expedita nisi sapiente velit. Cupiditate,
+          deserunt officiis?
+        </p>
+      </div>
+
+      <div className="flex max-lg:flex-col mt-12 items-center justify-center gap-13">
+        {reviews.map((rev) => (
+          <div className="flex flex-col items-center justify-center  gap-6 w-[350px]">
+            <img src={rev.imgURL} alt="" className="rounded-full w-[120px]" />
+            <p className="text-gray-500">{rev.feedback}</p>
+            <div className="flex gap-4">
+              {" "}
+              <img src={star} alt="" />{" "}
+              <p className="text-slate-500">{rev.rating}</p>
+            </div>
+            <p className="font-bold text-xl">{rev.customerName}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/sections/subscribe.tsx
+++ src/sections/subscribe.tsx
@@ -1,5 +1,24 @@
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section className="flex flex-col gap-16 items-center justify-center">
+      <div>
+        <h1 className="text-2xl font-bold">
+          Sign Up for <span className="text-red-500">Updates </span> &
+          Newsletter
+        </h1>
+      </div>
+      <div className="rounded-[40px] flex px-5 py-3 justify-between border-1 w-[60%]">
+        <input
+          type="email"
+          placeholder="subscribe@nike.com"
+          className="focus:outline-none w-[40%] text-center"
+        />
+        <button className="bg-red-400 cursor-pointer hover:scale-110 px-3 py-3 rounded-[40px]">
+          Sign Up
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
