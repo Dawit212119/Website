76d116bc179b9a2a81d6f784850b6f04e82ae99b
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-08-04 18:58:18 +0300

    super quality section

--- src/sections/popularProduct.tsx
+++ src/sections/popularProduct.tsx
@@ -5,17 +5,24 @@ const PopularProduct = () => {
   return (
     <section
       id="prdoucts"
-      className="flex flex-col w-full gap-16 max-sm:gap-10 mt-12"
+      className="flex flex-col w-full gap-30 max-sm:gap-10 mt-20 min-h-screen"
     >
       <div className="mb-10">
-        <h1 className="text-3xl font-serif ">
+        <h1 className="text-[50px] font-semibold ">
           Our <span className="text-red-500 ">Popular</span> Products
         </h1>
-        <p className="max-w-prose text-gray-500 ">
+        <p className="max-w-prose mt-5 text-gray-500 ">
           yeah these are the most well know products you used!
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-10 mx-auto">
+      <div className="grid grid-cols-4 gap-10 mx-auto mt-20">
+        {products.map((product) => (
+          <div>
+            <PopularProducts product={product} />
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-4 gap-10 mx-auto mt-20">
         {products.map((product) => (
           <div>
             <PopularProducts product={product} />
--- src/sections/superQuality.tsx
+++ src/sections/superQuality.tsx
@@ -1,5 +1,35 @@
+import { bigShoe3, shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
-  return <div>superQuality</div>;
+  return (
+    <section className="flex justify-around items-center  gap-16 min-h-screen">
+      <div className="flex flex-col w-1/2 ">
+        <h1 className="text-4xl font-bold leading-normal max-sm:whitespace-nowrap">
+          We Provide You <span className="text-red-500">Super</span>
+          <br />
+          <span className="text-red-500">Quality</span> Shoes
+        </h1>
+        <div className="pt-8 max-w-prose text-gray-500">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+            deserunt veritatis modi tempora consequuntur soluta eius quas
+            necessitatibus. Velit dolore saepe magni aspernatur iste nihil
+            <br />{" "}
+          </p>
+
+          <p className="pt-6">officia quam deleniti? Ipsam, assumenda.</p>
+        </div>
+        <div className="flex items-center pl-[25px]">
+          <button className="bg-red-400 px-2 py-4  rounded-4xl text-white text-xl font-bold mt-15 w-[200px] hover:bg-red-500 hover:scale-110 focus:outline-none focus:ring-amber-300">
+            View Details
+          </button>{" "}
+        </div>
+      </div>
+      <div className="flex-1 max-w-prose">
+        <img src={shoe8} className="w-fit h-[700px]" />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
