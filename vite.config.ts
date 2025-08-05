85fec71eeb1f7305f3e94e0fb70c4549eae99b49
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-08-05 10:58:03 +0300

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  server: {
+    allowedHosts: ["website-6fb1.onrender.com"],
+  },
 });
